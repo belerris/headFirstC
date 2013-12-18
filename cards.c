056386d4dd65cc7e9f9ed773da5dfd5f53491ef6
Micheal Reilly <ratmreilly@gmail.com>
2013-12-18 13:30:56 +0000

    initial commit

---
+++ cards.c
@@ -0,0 +1,37 @@
+/*
+ * First head first c program
+ */
+#include <stdio.h>
+#include <stlib.h>
+
+int main()
+{
+    char card_name[3];
+    int val = 0;
+    puts("Enter the card_name: ");
+    scanf("%2s", card_name);
+    if (card_name[0] == 'K')
+    {
+	val = 10;
+    }
+    else if(card_name[0] == 'Q')
+    {
+	val = 10;
+    }
+    else if(card_name[0] == 'J')
+    {
+	val = 10;
+    }
+    else if(card_name[0] == 'A')
+    {
+	val = 11;
+    }
+    else
+    {
+	val = atoi(card_name);
+    }
+    printf("The card value is: %i\n", val);
+
+    return 0;
+
+    
