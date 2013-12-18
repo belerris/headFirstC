5b80e853b2d4420dff1a436b8675fc036dc5e44f
Micheal Reilly <ratmreilly@gmail.com>
2013-12-18 14:42:22 +0000

    Indent content in loop and fixed if conidtion on count

--- cards.c
+++ cards.c
@@ -10,38 +10,40 @@ int main()
     int val = 0;
     int count = 0;
     while(card_name[0] != 'X'){
-    puts("Enter the card_name: ");
-    scanf("%2s", card_name);
-    switch(card_name[0])
-    {
-	case 'K':
-	case 'Q':
-	case 'J':
-	    val = 10;
-	    break;
-	case 'A':
-	    val = 11;
-	    break;
-	case 'X':
-	    continue;
-	default:
-	    val = atoi(card_name);
-	    if((val<1) || (val>10))
-	    {
-	    puts("I don't understand that value");
-	    continue;
-	    }
-    }
-    if(val>=3 || val <= 6)
-    {
-	puts("Count has gone up");
-	count++;
-    }
-    else if(val>=10){
-	puts("Count has gone down");
-	count--;
-    }
-    printf("The card value is: %i\n", val);
+	puts("Enter the card_name: ");
+    	scanf("%2s", card_name);
+    	switch(card_name[0])
+    	{
+    	    case 'K':
+    	    case 'Q':
+    	    case 'J':
+    	        val = 10;
+    	        break;
+    	    case 'A':
+    	        val = 11;
+    	        break;
+    	    case 'X':
+    	        continue;
+    	    default:
+    	        val = atoi(card_name);
+    	        if((val<1) || (val>10))
+    	        {
+    	        puts("I don't understand that value");
+    	        continue;
+    	        }
+    	}
+    	if(val>=3 && val <= 6)
+    	{
+    	    puts("Count has gone up");
+    	    count++;
+    	}
+    	else if(val==10 || val==11)
+	{
+    	    puts("Count has gone down");
+    	    count--;
+    	}
+    	printf("The card value is: %i\n", val);
+    	printf("The count value is: %i\n", count);
     }
     return 0;
 }
