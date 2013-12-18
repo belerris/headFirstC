e5236b52568d8fe3a904aee60abbfd726034788d
Micheal Reilly <ratmreilly@gmail.com>
2013-12-18 14:32:28 +0000

    Added loop and card counter

--- cards.c
+++ cards.c
@@ -8,34 +8,41 @@ int main()
 {
     char card_name[3];
     int val = 0;
+    int count = 0;
+    while(card_name[0] != 'X'){
     puts("Enter the card_name: ");
     scanf("%2s", card_name);
     switch(card_name[0])
     {
 	case 'K':
-	    val = 10;
-	    break;
 	case 'Q':
-	    val = 10;
-	    break;
 	case 'J':
 	    val = 10;
 	    break;
 	case 'A':
 	    val = 11;
 	    break;
+	case 'X':
+	    continue;
 	default:
 	    val = atoi(card_name);
+	    if((val<1) || (val>10))
+	    {
+	    puts("I don't understand that value");
+	    continue;
+	    }
     }
     if(val>=3 || val <= 6)
     {
 	puts("Count has gone up");
+	count++;
     }
     else if(val>=10){
 	puts("Count has gone down");
+	count--;
     }
     printf("The card value is: %i\n", val);
-
+    }
     return 0;
 }
 
